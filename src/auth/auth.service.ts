7eda09eec38d7ddc310d6c3c2054a30e39af9763
Eduardo Silva <eduardosilvajs.py@gmail.com>
2025-08-15 14:52:35 -0400

    Foi tenta coisa que nem da para comitar direito

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { UserService } from 'src/user/user.service';
+import { HashingService } from 'src/common/hashing/hashing.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './types/jwt-payload.type';
 
 @Injectable()
 export class AuthService {
-  checkLogin(LoginDto: LoginDto) {
-    console.log(typeof LoginDto, LoginDto);
-    return LoginDto;
+  constructor(
+    private readonly userService: UserService,
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async checkLogin(LoginDto: LoginDto) {
+    const user = await this.userService.findByEmail(LoginDto.email);
+    const error = new UnauthorizedException('Usuário ou senha inválidos');
+
+    if (!user) {
+      throw error;
+    }
+    const isPasswordValid = await this.hashingService.compare(
+      LoginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw error;
+    }
+
+    const JwtPayload: JwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+    const accessToken = await this.jwtService.signAsync(JwtPayload);
+
+    user.forceLogout = false;
+    await this.userService.save(user);
+
+    return { accessToken };
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from 'src/common/common.module';
+import { User } from './entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User]), CommonModule],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
